3006a5323b0e6dcab3dcb79033227d545438d9b0
penghao <hao.peng@nx-engine.com>
2019-04-29 15:02:21 +0800

    修改注解，@Component生成的对象是不同的，而@Configuration生成的对象是同一个

--- src/main/java/com/example/demo/config/KafkaConfig.java
+++ src/main/java/com/example/demo/config/KafkaConfig.java
@@ -8,6 +8,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -23,7 +24,7 @@ import java.util.Map;
  * @Date 2019/4/29 11:21
  * @Version 1.0
  **/
-@Component
+@Configuration
 public class KafkaConfig {
 
     @Autowired
