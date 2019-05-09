5556c08483fe7c40c7185d6498135d5b294a4ee6
penghao <hao.peng@nx-engine.com>
2019-05-09 17:54:57 +0800

    增加redis分布式锁的概念

--- src/main/java/com/example/demo/util/RedisLock.java
+++ src/main/java/com/example/demo/util/RedisLock.java
@@ -1,5 +1,6 @@
 package com.example.demo.util;
 
+import com.alibaba.excel.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -10,10 +11,41 @@ public class RedisLock {
     @Autowired
     private StringRedisTemplate redisTemplate;
 
+    /**
+     *  加锁
+     * @param key
+     * @param value 当前时间+TIMEOUT时间
+     * @return
+     */
     public boolean lock(String key, String value) {
         if (redisTemplate.opsForValue().setIfAbsent(key,value)) {
-
+            return true;
+        }
+        String currentValue = redisTemplate.opsForValue().get(key);
+        //如果锁过期
+        if (!StringUtils.isEmpty(currentValue) && Long.parseLong(currentValue) < System.currentTimeMillis()) {
+            //获取上一个锁的时间
+            String oldValue = redisTemplate.opsForValue().getAndSet(key,value);
+            if (!StringUtils.isEmpty(oldValue) && oldValue.equals(currentValue)) {
+                return true;
+            }
         }
         return false;
+    }
+
+    /**
+     * 解锁
+     * @param key
+     * @param value
+     */
+    public void unlock(String key,String value) {
+        try{
+            String currentValue = redisTemplate.opsForValue().get(key);
+            if (!StringUtils.isEmpty(currentValue) && currentValue.equals(value)) {
+                redisTemplate.opsForValue().getOperations().delete(key);
+            }
+        } catch(Exception e) {
+            log.error("redis分布式锁 解锁异常，{}",e);
+        }
     }
 }
