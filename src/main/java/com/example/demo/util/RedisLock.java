a8b6011eb69556aa49c8d6df69813b9fd665f969
penghao <hao.peng@nx-engine.com>
2019-05-09 18:02:28 +0800

    增加redis分布式锁的概念

--- src/main/java/com/example/demo/util/RedisLock.java
+++ src/main/java/com/example/demo/util/RedisLock.java
@@ -18,13 +18,21 @@ public class RedisLock {
      * @return
      */
     public boolean lock(String key, String value) {
+        //1.第一步，如果主键不存在，那么直接加锁，返回成功
         if (redisTemplate.opsForValue().setIfAbsent(key,value)) {
             return true;
         }
+        //2.如果第一步加锁失败，那么拿该主键的过期时间
         String currentValue = redisTemplate.opsForValue().get(key);
-        //如果锁过期
+        //2.1 如果锁过期
         if (!StringUtils.isEmpty(currentValue) && Long.parseLong(currentValue) < System.currentTimeMillis()) {
-            //获取上一个锁的时间
+            //2.2 获取上一个锁的时间：这里比如来了两个线程：A,B，并且他们的过期时间都为C
+            /**
+             * 这里只可能一个线程先进到getAndSet方法，
+             * 比如A先拿到，那么oldvalue==currentValue，并且A将自己的过期时间C设置到了该主键中
+             * 那么当B线程调用该方法时，oldvalue是==C的，所以B线程拿不到该锁
+             * 这样就保证了只有A这一个线程拿到锁
+             */
             String oldValue = redisTemplate.opsForValue().getAndSet(key,value);
             if (!StringUtils.isEmpty(oldValue) && oldValue.equals(currentValue)) {
                 return true;
