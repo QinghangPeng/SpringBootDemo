77209df8fdf4b34af0154dce285d0f0088d65077
penghao <hao.peng@nx-engine.com>
2019-06-21 10:55:30 +0800

    :sparkles: 增加利用线程实现异步请求功能

--- src/main/java/com/example/demo/controller/AsynchronousController.java
+++ src/main/java/com/example/demo/controller/AsynchronousController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.Response;
 import com.example.demo.service.AsynchronousService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -36,6 +37,16 @@ public class AsynchronousController {
         list.add(s);
         log.info("=========>" + Thread.currentThread().getName());
         return Response.success("异步，正在解析......");
+    }
+
+    @ApiOperation(value = "异步请求：printSomething方法不影响主线程的返回",notes = "适合发起一个请求，给用户迅速反应，然后在后台继续运算的操作")
+    @RequestMapping(value = "testExecutor",method = RequestMethod.GET)
+    public Response testExecutor() {
+        List<String> list = new ArrayList<>();
+        String s = service.getName();
+        list.add(s);
+        log.info("=========>" + Thread.currentThread().getName());
+        return Response.success(list);
     }
 
 }
