6299945a6142ec7f05423d3b4e32d1e218d892e5
QinghangPeng <voyagejk.peng@gmail.com>
2019-06-21 12:24:34 +0800

    :sparkles: 新增mongo聚合查询 针对springboot不同版本的不同实现方法

--- src/main/java/com/example/demo/dao/MongoDao.java
+++ src/main/java/com/example/demo/dao/MongoDao.java
@@ -57,4 +57,142 @@ public class MongoDao {
         System.out.println("================================"+list.size());
         return null;
     }
+
+    /**
+     *   springboot 2.x 版本  mongo聚合查询写法
+     *   以及 match 下面 同时包含 "$and" "$or" 的写法
+     *   在此做一个记录
+     * @param from
+     * @param to
+     * @param vin
+     * @return
+     */
+    public List<Document> calCount(Long from,Long to,String vin) {
+        List<? extends Bson> pipeline = Arrays.asList(
+                new Document()
+                        .append("$match", new Document()
+                                .append("$and", Arrays.asList(
+                                        new Document()
+                                                .append("tBoxTime", new Document()
+                                                        .append("$gte", from)
+                                                        .append("$lte", to)
+                                                )
+                                        )
+                                )
+                                .append("$or", Arrays.asList(
+                                        new Document()
+                                                .append("vin", vin)
+                                                .append("vehicleOverspeedAlarmSignal", 1),
+                                        new Document()
+                                                .append("vin", vin)
+                                                .append("engineOverspeedAlarmSignal", 1),
+                                        new Document()
+                                                .append("vin", vin)
+                                                .append("rapidDecelerationAlarmSignal", 1),
+                                        new Document()
+                                                .append("vin", vin)
+                                                .append("coastingEndAlarmSignal", 1)
+                                        )
+                                )
+                        ),
+                new Document()
+                        .append("$group", new Document()
+                                .append("_id", 0)
+                                .append("overSpeedCount", new Document()
+                                        .append("$sum", "$vehicleOverspeedAlarmSignal")
+                                )
+                                .append("overEngineCount", new Document()
+                                        .append("$sum", "$engineOverspeedAlarmSignal")
+                                )
+                                .append("decelerationCount", new Document()
+                                        .append("$sum", "$rapidDecelerationAlarmSignal")
+                                )
+                                .append("coastingCount", new Document()
+                                        .append("$sum", "$coastingEndAlarmSignal")
+                                )
+                        ),
+                new Document()
+                        .append("$project", new Document()
+                                .append("_id", 0)
+                                .append("overSpeedCount", "$overSpeedCount")
+                                .append("overEngineCount", "$overEngineCount")
+                                .append("decelerationCount", "$decelerationCount")
+                                .append("coastingCount", "$coastingCount")
+                        )
+        );
+        AggregateIterable iterable = mongoTemplate.getCollection("event_data").aggregate(pipeline);
+        List<Document> list = new ArrayList<>();
+        iterable.forEach((Block<Document>) document -> {
+            list.add(document);
+            //格式化为对应实体的写法 记录
+//           Xx xx = mongoTemplate.getConverter().read(Xx.class,document);
+        });
+        return list;
+    }
+
+    /**
+     *  springboot 1.x 版本  mongo聚合查询写法
+     *  以及 match 下面 同时包含 "$and" "$or" 的写法
+     *  在此做一个记录
+     */
+    /*public BasicDBObject calCount(Long from,Long to,String vin) {
+        List<DBObject> pipeline = Arrays.asList(
+                new BasicDBObject()
+                        .append("$match", new BasicDBObject()
+                                .append("$and", Arrays.asList(
+                                        new BasicDBObject()
+                                                .append("tBoxTime", new BasicDBObject()
+                                                        .append("$gte", from)
+                                                        .append("$lte", to)
+                                                )
+                                        )
+                                )
+                                .append("$or", Arrays.asList(
+                                        new BasicDBObject()
+                                                .append("vin", vin)
+                                                .append("vehicleOverspeedAlarmSignal", 1),
+                                        new BasicDBObject()
+                                                .append("vin", vin)
+                                                .append("engineOverspeedAlarmSignal", 1),
+                                        new BasicDBObject()
+                                                .append("vin", vin)
+                                                .append("rapidDecelerationAlarmSignal", 1),
+                                        new BasicDBObject()
+                                                .append("vin", vin)
+                                                .append("coastingEndAlarmSignal", 1)
+                                        )
+                                )
+                        ),
+                new BasicDBObject()
+                        .append("$group", new BasicDBObject()
+                                .append("_id", 0)
+                                .append("overSpeedCount", new BasicDBObject()
+                                        .append("$sum", "$vehicleOverspeedAlarmSignal")
+                                )
+                                .append("overEngineCount", new BasicDBObject()
+                                        .append("$sum", "$engineOverspeedAlarmSignal")
+                                )
+                                .append("decelerationCount", new BasicDBObject()
+                                        .append("$sum", "$rapidDecelerationAlarmSignal")
+                                )
+                                .append("coastingCount", new BasicDBObject()
+                                        .append("$sum", "$coastingEndAlarmSignal")
+                                )
+                        ),
+                new BasicDBObject()
+                        .append("$project", new BasicDBObject()
+                                .append("_id", 0)
+                                .append("overSpeedCount", "$overSpeedCount")
+                                .append("overEngineCount", "$overEngineCount")
+                                .append("decelerationCount", "$decelerationCount")
+                                .append("coastingCount", "$coastingCount")
+                        )
+        );
+        Cursor cursor = mongoTemplate.getCollection("event_data").aggregate(pipeline, OPTIONS);
+        if (cursor.hasNext()) {
+            BasicDBObject document = (BasicDBObject) cursor.next();
+            return document;
+        }
+        return null;
+    }*/
 }
