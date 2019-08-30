1487dc37a40ff68dfd83953332cbb6c2e943d338
QinghangPeng <voyagejk.peng@gmail.com>
2019-08-30 16:08:44 +0800

    增加shard的两种shard key的描述和优缺点

--- src/main/java/com/example/demo/dao/admin/AdminMongoDao.java
+++ src/main/java/com/example/demo/dao/admin/AdminMongoDao.java
@@ -25,7 +25,13 @@ public class AdminMongoDao {
     public void createShard(String colName) {
         Document object = new Document();
         object.put("shardCollection","new_report." + colName);
+        /**
+         *  mongo shardkey  主流分为两种：
+         *   ranged sharding:  片键组合，会并且只会创建“升序”的索引，散列可能不大，会集中到一个shard上
+         *   hashed sharding: 只能一个字段("vin","hashed") 组合，散列范围大，但是查询的时候可能会广播所有shard，速度降低
+         */
         object.put("key",new Document().append("vin",1).append("tBoxTime",1));
-        adminTemplate.executeCommand(object);
+        Document result = adminTemplate.executeCommand(object);
+        log.info("create shard result:{}",result.toJson());
     }
 }
