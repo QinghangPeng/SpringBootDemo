3f1b525cfd944d6f70236fc6f17ebfb5b4db326d
stealhao <penghao@zhiyuanhui.com>
2021-01-28 14:14:56 +0800

    redislock

--- src/main/java/com/example/demo/util/RedisLock.java
+++ src/main/java/com/example/demo/util/RedisLock.java
@@ -56,4 +56,39 @@ public class RedisLock {
             log.error("redis分布式锁 解锁异常，{}",e);
         }
     }
+
+    public boolean tryLock(String targetId,String timeStamp){
+        if(redisTemplate.opsForValue().setIfAbsent(targetId,timeStamp)){
+            // 对应setnx命令，可以成功设置,也就是key不存在
+            return true;
+        }
+
+        // 判断锁超时 - 防止原来的操作异常，没有运行解锁操作  防止死锁
+        String currentLock = redisTemplate.opsForValue().get(targetId);
+        // 如果锁过期 currentLock不为空且小于当前时间
+        if(org.apache.commons.lang.StringUtils.isNotBlank(currentLock) && Long.parseLong(currentLock) < System.currentTimeMillis()){
+            // 获取上一个锁的时间value 对应getset，如果lock存在
+            String preLock =redisTemplate.opsForValue().getAndSet(targetId,timeStamp);
+
+            // 假设两个线程同时进来这里，因为key被占用了，而且锁过期了。获取的值currentLock=A(get取的旧的值肯定是一样的),两个线程的timeStamp都是B,key都是K.锁时间已经过期了。
+            // 而这里面的getAndSet一次只会一个执行，也就是一个执行之后，上一个的timeStamp已经变成了B。只有一个线程获取的上一个值会是A，另一个线程拿到的值是B。
+            if(org.apache.commons.lang.StringUtils.isNotBlank(preLock) && preLock.equals(currentLock) ){
+                // preLock不为空且preLock等于currentLock，也就是校验是不是上个对应的商品时间戳，也是防止并发
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void tryUnlock(String target,String timeStamp){
+        try {
+            String currentValue = redisTemplate.opsForValue().get(target);
+            if(org.apache.commons.lang.StringUtils.isNotBlank(currentValue) && currentValue.equals(timeStamp) ){
+                // 删除锁状态
+                redisTemplate.opsForValue().getOperations().delete(target);
+            }
+        } catch (Exception e) {
+            log.error("警报！警报！警报！解锁异常{}",e);
+        }
+    }
 }
